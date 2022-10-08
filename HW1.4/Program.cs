bcc0036aba1ea5ea7a42d7b5623b477b8936af8e
Beslan <biozdoev@gmail.com>
2022-10-08 12:07:17 +0300

    Fourth task

--- HW1.4/Program.cs
+++ HW1.4/Program.cs
@@ -3,7 +3,7 @@ int numberA = int.Parse(Console.ReadLine());
 double numberA_2 = numberA % 2;
 if (numberA_2==0)
 {
-Console.WriteLine("число четное");
+Console.WriteLine("Число четное");
 }
 else
 {
