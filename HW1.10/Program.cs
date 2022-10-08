705302a645369e7ef558846bcf0cb787e4dc5509
Beslan <biozdoev@gmail.com>
2022-10-08 12:32:40 +0300

    Tennth task

---
+++ HW1.10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+int s = 2;
+int m = 1;
+while (s > 1) {
+     m *= 10;
+     s--;
+}
+int t = (x % m) + (x / (m * 10)) * m;
+Console.WriteLine(t);
