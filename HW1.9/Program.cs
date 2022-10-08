4dd74f17b340173df5ea5545ede49a984b477cf3
Beslan <biozdoev@gmail.com>
2022-10-08 12:30:16 +0300

    Ninth task

---
+++ HW1.9/Program.cs
@@ -0,0 +1,15 @@
+﻿int numberA = new Random().Next(10,90);
+Console.Write ("Заданное число = ");
+Console.WriteLine (numberA);
+
+int a = numberA % 10;
+
+int b = numberA / 10 % 10;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+
+Console.Write("max = ");
+Console.WriteLine (max);
