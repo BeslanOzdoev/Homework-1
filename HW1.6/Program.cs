a4667286d61ade80d6844e969f92cd57f7ffb502
Beslan <biozdoev@gmail.com>
2022-10-08 12:25:25 +0300

    Sixth task

---
+++ HW1.6/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+ 
+class Program {
+ public static int Main() {
+  for(int i=0; i<=20; i++) {
+   if(i%2 == 0) {
+    Console.Write("{0} ", i);
+   }
+  }
+  Console.ReadKey();
+  return 0;
+ }
+}
