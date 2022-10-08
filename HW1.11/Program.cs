3e8f34766e0b7c7fb43e29f2a8fc43cf65edad46
Beslan <biozdoev@gmail.com>
2022-10-08 12:54:29 +0300

    Eleventh task

---
+++ HW1.11/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите число ");
+double x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число ");
+double y = Convert.ToInt32(Console.ReadLine());
+double numberA = x / y;
+
+double numberB = numberA - Math.Round(numberA);
+
+if (numberB==0)
+{
+Console.WriteLine("Число кратко заданному");
+}
+else
+{
+Console.Write("Число некратно, остаток = ");
+Console.WriteLine(numberB);
+}
+
+
+
