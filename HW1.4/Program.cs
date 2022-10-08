37ab77c38598ae05a4a04b364e58445cbddcd790
Beslan <biozdoev@gmail.com>
2022-10-08 12:05:16 +0300

    Fourth task

---
+++ HW1.4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите целое число ");
+int numberA = int.Parse(Console.ReadLine());
+double numberA_2 = numberA % 2;
+if (numberA_2==0)
+{
+Console.WriteLine("число четное");
+}
+else
+{
+Console.WriteLine("Число нечетное");
+}
