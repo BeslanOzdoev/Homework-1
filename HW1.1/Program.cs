e1da21fb664f3187f53e623bf9e645e427cd3723
Beslan <biozdoev@gmail.com>
2022-10-08 11:47:45 +0300

    First task

---
+++ HW1.1/Program.cs
@@ -0,0 +1,18 @@
+﻿int numberA = 4;
+int numberB = 16;
+
+if (numberA < 0)
+{
+     Console.WriteLine($"Число {numberA} отрицательно, корня квадратного не имеет");
+}
+else
+{
+        if (numberB != Math.Sqrt(numberA))
+        {
+              Console.WriteLine($"Число {numberA} является квадратным корнем {numberB}");
+        }
+        else
+        {
+                  Console.WriteLine($"Число {numberA} не является квадратным корнем {numberB}");
+        }
+}
