c1463d1f13fd335b39c3c22f8230047436f648b0
Beslan <biozdoev@gmail.com>
2022-10-08 12:28:59 +0300

    Eighth task

---
+++ HW1.8/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите трехзначное число ");
+int numberA = int.Parse(Console.ReadLine());
+
+int numberA_2 = numberA / 10 % 10;
+
+Console.Write("Второй элемент числа ");
+Console.WriteLine(numberA_2);
